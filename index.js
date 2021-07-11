d1c0c45947210a3e472d7bd6c4074bff0d77ab09
Twyll <twyllblackleaf@gmail.com>
2021-07-11 18:58:38 -0400

    inquirer prompt asking what to do

--- index.js
+++ index.js
@@ -3,10 +3,57 @@ const inquirer = require("inquirer");
 const cTable = require("console.table");
 const db = require("./db/connection");
 const sqlQueries = require("./utils/sqlQueries");
+const questions = require("./utils/questions");
 
+const askWhatDo = function() {
+    inquirer.prompt(questions.whatDo)
+        .then(({ whatDo }) => {
+            if (whatDo === "View all departments") {
+                db.query(sqlQueries.departments, (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log(rows);
+                });
+            } else if (whatDo === "View all roles") {
+                db.query(sqlQueries.roles, (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log(rows);
+                });
+            } else if (whatDo === "View all employees") {
+                db.query(sqlQueries.employees, (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log(rows);
+                });
+            } else if (whatDo === "Add a department") {
 
+            } else if (whatDo === "Add a role") {
+                
+            } else if (whatDo === "Add an employee") {
 
-db.query(sqlQueries.employees, (err, rows) => {
+            } else if (whatDo === "Update an employee role") {
+
+            } else {
+                console.log("Error: need to choose an option");
+                return;
+            }
+        });
+}
+
+const init = function() {
+    askWhatDo();
+}
+
+init();
+
+/* db.query(sqlQueries.employees, (err, rows) => {
     if (err) {
         console.log(err);
         return;
@@ -16,4 +63,4 @@ db.query(sqlQueries.employees, (err, rows) => {
     rows.forEach(row => {
         console.log(row.id);
     });
-})
+}) */
---
+++ utils/questions.js
@@ -0,0 +1,18 @@
+const questions = {
+    whatDo: {
+        type: "list",
+        name: "whatDo",
+        message: "What would you like to do?",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role"
+        ]
+    }
+}
+
+module.exports = questions;
